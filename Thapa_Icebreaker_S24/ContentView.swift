f96ad5f7587b6908281597e405bcdce686b956f9
sudeptech <118206782+sudeptech@users.noreply.github.com>
2024-02-06 18:05:49 -0500

    Project initialization

---
+++ Thapa_Icebreaker_S24/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Thapa_Icebreaker_S24
+//
+//  Created by Sudip Thapa on 2/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Thapa_Icebreaker_S24/Thapa_Icebreaker_S24App.swift
@@ -0,0 +1,17 @@
+//
+//  Thapa_Icebreaker_S24App.swift
+//  Thapa_Icebreaker_S24
+//
+//  Created by Sudip Thapa on 2/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Thapa_Icebreaker_S24App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
