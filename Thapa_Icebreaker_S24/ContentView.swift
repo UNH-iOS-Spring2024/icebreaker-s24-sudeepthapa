e2a86245c4ea1dae591ec17bf24215c1d3341136
sudeptech <118206782+sudeptech@users.noreply.github.com>
2024-02-24 17:45:09 -0500

    UI and firebase integrated

--- Thapa_Icebreaker_S24/ContentView.swift
+++ Thapa_Icebreaker_S24/ContentView.swift
@@ -6,16 +6,136 @@
 //
 
 import SwiftUI
+import FirebaseFirestore
 
 struct ContentView: View {
+    let db = Firestore.firestore()
+    
+    @State var txtFirstName: String = ""
+    @State var txtLastName: String = ""
+    @State var txtPrefName: String = ""
+    @State var txtAnswer: String = ""
+    @State var txtQuestion: String = ""
+    
+    @State var questions = [Question]()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            Text("Icebreaker App")
+                .font(.system(size: 40))
+                .bold()
+            Text("Built with SwiftUI")
+            
+            TextField("First Name", text: $txtFirstName)
+                .textFieldStyle(.roundedBorder)
+                .border(.secondary)
+                .font(.system(size: 18))
+            
+            
+            TextField("Last Name", text: $txtLastName)
+                .textFieldStyle(.roundedBorder)
+                .border(.secondary)
+                .font(.system(size: 18))
+            
+            
+            
+            TextField("Preferred Name", text: $txtPrefName)
+                .textFieldStyle(.roundedBorder)
+                .border(.secondary)
+                .font(.system(size: 18))
+            
+            
+            
+            Button(action: { setRandomQuestion()
+            }){
+                Text("Get a new random question")
+                    .font(.system(size: 24))
+            }
+            if(txtQuestion != ""){
+                Text(txtQuestion)
+                    .font(.system(size: 20)).padding()
+            }
+
+            
+            TextField("Answer", text: $txtAnswer)
+                .textFieldStyle(.roundedBorder)
+                .border(.secondary)
+                .font(.system(size: 18))
+            
+            
+            Button(action: {
+                if(txtAnswer != "") {
+                    writeStudentToFirebase()
+                }
+                resetTextFields()
+                
+            })
+            {
+                Text("Submit")
+                    .font(.system(size: 24)).padding(10)
+            }
+        }
+        .font(.largeTitle)
+        .autocorrectionDisabled(true)
+        .padding()
+        .onAppear(){
+            getQuestionsFromFirebase()
+        }
+    }
+    
+    func setRandomQuestion(){
+        print("Set random question pressed")
+        let newQuestion = questions.randomElement()?.text
+        if(newQuestion != nil){
+            self.txtQuestion = newQuestion!
+        }
+    }
+    
+    func getQuestionsFromFirebase(){
+        db.collection("questions")
+            .getDocuments() {  (querySnapshot,err) in
+                if let err = err {
+                    print("Error getting documents: \(err)")
+                } else {
+                    for document in querySnapshot!.documents {
+                        if let question = Question(id: document.documentID, data: document.data()){
+                            self.questions.append(question)
+                        }
+                    }
+                }
+                
+            }
+    }
+    
+    func resetTextFields(){
+        txtFirstName = ""
+        txtLastName = ""
+        txtPrefName = ""
+        txtAnswer = ""
+        txtQuestion = ""
+    }
+    
+    func writeStudentToFirebase(){
+        let data = ["first_name" : txtFirstName,
+                    "last_name" : txtLastName,
+                    "pref_name" : txtPrefName,
+                    "question"  : txtQuestion,
+                    "answer"    : txtAnswer,
+                    "class"     : "ios-spring2024"] as [String : Any]
+        
+        var ref: DocumentReference? = nil
+        ref = db.collection("students")
+            .addDocument(data: data) { err in
+                if let err = err {
+                    print("Error adding document: \(err)")
+                } else {
+                    print("Document added with ID: \(ref!.documentID)")
+                }
+            }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+#Preview {
+    ContentView()
 }
